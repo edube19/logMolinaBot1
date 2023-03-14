ab45cb18798e678b5195cd6ea7fb364438b2b0c7
edube19 <92874291+edube19@users.noreply.github.com>
2023-03-14 08:58:36 -0500

    Add files via upload

--- login_logistica.py
+++ login_logistica.py
@@ -1,19 +1,22 @@
 from playwright.sync_api import Playwright, sync_playwright
 
 from recursos import *
-import time
 
 def run(playwright: Playwright) -> None:
     
     try:
+        filename = 'datos.xlsx'
         #datos
-        articulo = 'QUESO EDAM'
+        #articulo = 'QUESO EDAM'
         factura = '20601452651'
         cod_serie = 'E001'
         num_serie = '12654'
-        destino = '102' #almacen cocina
-        cantidad = '5'#tiene q ser string
-        precio_unitario = '45' #tiene que ser string
+        destino = '102'
+        #cantidad = '5'
+        #precio_unitario = '45.50' 
+        lista_productos = extraer_datos(filename)
+        long_lista_productos = len(lista_productos)
+        print('cantidad de items ',str(long_lista_productos))
 
         pagina_base = 'http://190.187.186.218/index.aspx'
         #Creacion del contexto de la pag web
@@ -23,11 +26,15 @@ def run(playwright: Playwright) -> None:
         #entrando a la pagina web
         #wait_until ='networkidle'
         page.goto(pagina_base, wait_until ='networkidle')
-
+        
         #page.click('#loginId')
         ingreso(page)
-        main(page,articulo,factura,cod_serie,num_serie,destino,cantidad,precio_unitario)
-        
+        '''for producto in lista_productos:
+            articulo = producto['producto']
+            cantidad = producto['cantidad']
+            precio_unitario = producto['precio_unitario']
+            main(page,articulo,factura,cod_serie,num_serie,destino,cantidad,precio_unitario)'''
+        main(page,lista_productos,long_lista_productos,factura,cod_serie,num_serie,destino)
         context.close()
         browser.close()
 
--- recursos.py
+++ recursos.py
@@ -1,5 +1,5 @@
 import time
-
+from openpyxl import workbook,load_workbook
 #ingreso de las credenciales para el LOGIN
 def ingreso(page):
     page.get_by_label("Usuario :").click()
@@ -11,7 +11,7 @@ def ingreso(page):
     print('presiono el boton')
 
 #ingreso a la seccion de ordenes de compra y registro de los datos
-def main(page,articulo,factura,cod_serie,num_serie,destino,cantidad,precio_unitario):
+def main(page,lista_productos,long_lista_productos,factura,cod_serie,num_serie,destino):
     page.wait_for_url("http://190.187.186.218/Default.aspx")
     page.get_by_role("link", name="Compras").click()
     page.wait_for_url("http://190.187.186.218/Compras/Default.aspx")
@@ -31,32 +31,78 @@ def main(page,articulo,factura,cod_serie,num_serie,destino,cantidad,precio_unita
     page.locator("input[name=\"ctl00\\$Main\\$num_doc_ref\"]").fill(num_serie)#NUMERO DE SERIE
     print('lleno el codigo de factura')
     page.locator("select[name=\"ctl00\\$Main\\$ALMACEN\"]").select_option(destino)
-    page.get_by_role("button", name="Adicionar Item").click()
-    page.locator("input[name=\"ctl00\\$Main\\$descri_articulo\"]").click()
-    print('ingresando el producto ',articulo)
-    page.keyboard.type(articulo, delay=200)
-    #page.locator("input[name=\"ctl00\\$Main\\$descri_articulo\"]").fill(articulo_recortado,timeout=30000)
-    #page.locator("input[name=\"ctl00\\$Main\\$descri_articulo\"]").click()
-    time.sleep(3)
-    page.get_by_text(articulo).click()
-    time.sleep(3)
-    print('ingresando la cantidad')
-    page.locator("input[name=\"ctl00\\$Main\\$cantidad\"]").fill(cantidad)
-    page.locator("input[name=\"ctl00\\$Main\\$cantidad\"]").press("Tab")
-    print('ingresando el precio unitario')
-    page.get_by_role("cell", name="Precio Unitario").click()
-    page.locator("input[name=\"ctl00\\$Main\\$valor_uni\"]").click()
-    time.sleep(3)
-    page.locator("input[name=\"ctl00\\$Main\\$valor_uni\"]").fill(precio_unitario)
-    time.sleep(3)
-    page.locator("input[name=\"ctl00\\$Main\\$valor_uni\"]").press("Tab")
-    page.get_by_role("button", name="ACEPTAR").click()
-    print('se añadio el producto ',articulo)
-    time.sleep(3)
-    page.locator("input[name=\"ctl00\\$Main\\$descri_articulo\"]").click()
-    page.locator('#Main_btnCerrar').click()
+    print('lleno los datos de la cabecera')
+    #i=0
+    #while i<long_lista_productos:
+    for i in range(long_lista_productos):
+        elemento_lista = lista_productos[i]
+        print(elemento_lista)
+        articulo = elemento_lista['producto']
+        cantidad = elemento_lista['cantidad']
+        precio_unitario = elemento_lista['precio_unitario']
+        page.get_by_role("button", name="Adicionar Item").click()
+        page.locator("input[name=\"ctl00\\$Main\\$descri_articulo\"]").fill("")
+        page.get_by_role("group", name="Orden de Compra").locator("table:has-text(\"Item : Cód.Articulo : Ingresar Codigo Cantidad : Ingresar Cantidad Precio Unitar\")").click()
+        page.locator("input[name=\"ctl00\\$Main\\$descri_articulo\"]").click()
+        print('ingresando el producto ',articulo)
+        page.keyboard.type(articulo, delay=200)
+        #page.locator("input[name=\"ctl00\\$Main\\$descri_articulo\"]").fill(articulo_recortado,timeout=30000)
+        #page.locator("input[name=\"ctl00\\$Main\\$descri_articulo\"]").click()
+        #time.sleep(3)
+        page.get_by_text(articulo).click()
+        time.sleep(1)
+        print('ingresando la cantidad')
+        page.locator("input[name=\"ctl00\\$Main\\$cantidad\"]").fill(str(cantidad))
+        page.locator("input[name=\"ctl00\\$Main\\$cantidad\"]").press("Tab")
+        print('ingresando el precio unitario')
+        page.get_by_role("cell", name="Precio Unitario").click()
+        page.locator("input[name=\"ctl00\\$Main\\$valor_uni\"]").click()
+        time.sleep(1)
+        page.locator("input[name=\"ctl00\\$Main\\$valor_uni\"]").fill(str(precio_unitario))
+        time.sleep(1)
+        page.locator("input[name=\"ctl00\\$Main\\$valor_uni\"]").press("Tab")
+        page.get_by_role("button", name="ACEPTAR").click()
+        print('se añadio el producto ',articulo)
+        time.sleep(1)
+        page.locator("input[name=\"ctl00\\$Main\\$descri_articulo\"]").click()
+        page.locator('#Main_btnCerrar').click()
+        print('Acabo de ingresar los datos del producto ',articulo)
+        time.sleep(1)
+        
     #page.get_by_role("button", name="SALIR").click()
     print('saliendo ... ')
 
     time.sleep(3)
 
+def nro_filas_columnas(sheet):
+    num_rows = 0
+    num_cols = 0
+    # Itera a través de las filas y columnas de la hoja de Excel
+    for row in sheet.iter_rows():
+        num_rows += 1
+        for cell in row:
+            if cell.value:
+                num_cols = max(num_cols, cell.column)
+
+    # Imprime el número de filas y columnas que contienen datos
+    return num_rows,num_cols
+
+def extraer_datos(filename):
+    wb = load_workbook(filename)
+    sheet = wb.worksheets[0]#trabajar con la Hoja1
+    #nfilas,ncolumnas = nro_filas_columnas(sheet)
+    datos = {
+        'producto': [],
+        'cantidad': [],
+        'precio_unitario': [],
+    }
+    # Itera a través de las filas de la hoja de Excel
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        # Agrega los valores de cada columna al diccionario correspondiente
+        datos['producto'].append(row[0])
+        datos['cantidad'].append(row[1])
+        datos['precio_unitario'].append(row[2])
+
+    # Crea una lista de diccionarios con los valores de todas las columnas
+    datos = [dict(zip(datos, valores)) for valores in zip(*datos.values())]#esto es una lista
+    return datos
